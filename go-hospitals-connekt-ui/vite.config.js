fd73726805af2c17eb861b96f815f9a3eb2e7597
Surya <iamsuryavamsi@gmail.com>
2025-01-22 18:03:20 +0530

    Changes Done

--- go-hospitals-connekt-ui/vite.config.js
+++ go-hospitals-connekt-ui/vite.config.js
@@ -1,18 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-// import path from 'path';
-// import fs from 'fs';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
     port: 7778,
-    // https: {
-    //   key: fs.readFileSync(path.resolve('./gowork-ssl/gowork.key')),
-    //   cert: fs.readFileSync(path.resolve('./gowork-ssl/gowork.crt'))
-    // }
   },
   define: {
     global: 'window'
