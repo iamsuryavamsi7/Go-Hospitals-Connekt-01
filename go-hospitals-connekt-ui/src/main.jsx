ec3bc79893af343cf6d4e27879c1c0500c17ad60
Surya <iamsuryavamsi@gmail.com>
2025-01-22 18:27:18 +0530

    Changes Done

--- go-hospitals-connekt-ui/src/main.jsx
+++ go-hospitals-connekt-ui/src/main.jsx
@@ -31,9 +31,9 @@ const App = () => {
 
 createRoot(document.getElementById('root')).render(
 
-	// <BrowserRouter>
-	// 	<GoWorkRouter />
-	// </BrowserRouter>
-		<App />
+	<BrowserRouter>
+		<GoWorkRouter />
+	</BrowserRouter>
+		// <App />
 
 )
